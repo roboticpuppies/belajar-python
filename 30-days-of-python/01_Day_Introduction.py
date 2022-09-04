3832d872e3b5595d535667e9364724b7437b11e4
Mochammad Syaifuddin <roboticpuppies@gmail.com>
2022-09-04 20:57:18 +0700

    Create 01_Day_Introduction.py

---
+++ 30-days-of-python/01_Day_Introduction.py
@@ -0,0 +1,40 @@
+# Open the python interactive shell and do the following operations. The operands are 3 and 4.
+# addition
+print(3 + 4)
+# subtraction
+print(3 - 4)
+# multiplication
+print(3 * 4)
+# modulus
+print(3 % 4)
+# division
+print(3/4)
+# exponential
+print(3**4)
+# floor division
+print(3//4)
+"""
+Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+"""
+print("Yor Forger")
+print("SpyXFamily")
+print("Japan")
+print("I am enjoying 30 days of python. Help me.")
+
+"""
+Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+"""
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type('4 - 4j'))
+print(type(['Asabeneh', 'Python', 'Finland']))
